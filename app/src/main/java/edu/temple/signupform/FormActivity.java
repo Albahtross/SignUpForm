f692c2f7b654afe1894c927801207fb877137825
Albahtross <alexliu401@gmail.com>
2020-09-23 18:00:48 -0400

    button onclicklistener added, need to work on saveUserData method

--- app/src/main/java/edu/temple/signupform/FormActivity.java
+++ app/src/main/java/edu/temple/signupform/FormActivity.java
@@ -3,6 +3,10 @@ package edu.temple.signupform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class FormActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class FormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText PersonName = (EditText)findViewById(R.id.PersonName);
+        EditText EmailAddress=(EditText)findViewById(R.id.EmailAddress);
+        EditText Password=(EditText)findViewById(R.id.Password);
+        EditText PasswordAuth=(EditText)findViewById(R.id.PasswordAuth);
+        Button SignUpButton= findViewById(R.id.SignUp);
+
+        SignUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                saveUserData();
+            }
+        });
     }
+
+    void saveUserData(){
+        
+    }
+
 }
